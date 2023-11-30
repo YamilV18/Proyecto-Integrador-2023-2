25cccd5eaef4b4e76492484b15976be1b9967bc3
YamilV18 <118829287+YamilV18@users.noreply.github.com>
2023-11-30 00:24:59 -0500

    Prueba crud

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var categoriesRouter = require('./routes/categories');
 var productsRouter = require('./routes/products');
+var orderRouter = require('./routes/order');
 
 var app = express();
 
@@ -40,6 +41,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/categories', categoriesRouter);
 app.use('/products', productsRouter);
+app.use('/order', orderRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
